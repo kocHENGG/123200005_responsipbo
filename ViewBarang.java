e446e25ec219b67fe5090ff7ba017ed440313555
kocHENGG <100083446+kocHENGG@users.noreply.github.com>
2022-05-30 21:51:45 +0700

    Update ViewBarang.java

--- ViewBarang.java
+++ ViewBarang.java
@@ -9,6 +9,86 @@ package responsipbo;
  *
  * @author ACER
  */
-public class ViewBarang {
+import javax.swing.*;
+
+public class ViewBarang extends JFrame{
+  
+    JLabel ljudul = new JLabel("Input Barang");
+    JLabel lnama = new JLabel("Nama");
+    JLabel lmassa = new JLabel("Massa(gr)");
+    JLabel lharga = new JLabel("Harga Satuan");
+    
+    JTextField tfnama = new JTextField();
+    JTextField tfmassa = new JTextField();
+    JTextField tfharga = new JTextField();
+    
+    JButton btnSubmit = new JButton("Submit");
+    JButton btnReset = new JButton("Reset");
+    JButton btnKembali = new JButton("Kembali");
+
+    public ViewBarang() {
+        setTitle("Main Menu");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+        setSize(200,200);
+        
+        add(ljudul);
+        ljudul.setBounds(10, 10, 80, 20);
+        
+        add(lnama);
+        lnama.setBounds(10, 30, 80, 20);
+        add(tfnama);
+        tfnama.setBounds(90, 30, 80, 20);
+        
+        add(lmassa);
+        lmassa.setBounds(10, 50, 80, 20);
+        add(tfmassa);
+        tfmassa.setBounds(90, 50, 80, 20);
+        
+        add(lharga);
+        lharga.setBounds(10, 70, 80, 20);
+        add(tfharga);
+        tfharga.setBounds(90, 70, 80, 20);
+        
+        add(btnSubmit);
+        btnSubmit.setBounds(10, 100, 80, 20);
+        
+        add(btnReset);
+        btnReset.setBounds(90, 100, 80, 20);
+        
+        add(btnKembali);
+        btnKembali.setBounds(10, 130, 160, 20);
+    }
+    
+    public String getTfnama() {
+        String nama = tfnama.getText();
+        return nama;
+    }
+
+    public Double getTfmassa() {
+        if(tfmassa.getText().isEmpty()){
+            double massa = 0;
+            return massa;
+        }
+        else{
+            double massa = Double.parseDouble(tfmassa.getText());
+            return massa;
+        }
+            
+        
+    }
+
+    public Double getTfharga() {
+        if(tfharga.getText().isEmpty()){
+            double harga = 0;
+            return harga;
+        }
+        else{
+            double harga = Double.parseDouble(tfharga.getText());
+            return harga;
+        }
+        
+    }
     
 }
