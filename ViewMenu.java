79378dc6eb44dfdbebb8bc32177ede399e667138
kocHENGG <100083446+kocHENGG@users.noreply.github.com>
2022-05-30 21:52:42 +0700

    Update ViewMenu.java

--- ViewMenu.java
+++ ViewMenu.java
@@ -4,66 +4,32 @@
  * and open the template in the editor.
  */
 package responsipbo;
-
+import javax.swing.*;
 /**
  *
  * @author ACER
  */
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.*;
-import javax.swing.*;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class ViewMenu extends JFrame{
-    JButton btnLoginAdmin = new JButton("Tambah Barang");
-    JButton btnBeliSayur = new JButton("Lihat Barang");
+
+public class ViewMenu extends JFrame {
     JLabel judul = new JLabel("Main Menu");
-    
-    public ViewMenu()
-    {
-     setTitle("Menu Awal");
-     setDefaultCloseOperation(EXIT_ON_CLOSE);
-     setVisible(true);
-     setLayout(null);
-     setSize(400,300);
-     setLocation(500,100);
-     this.getContentPane().setBackground(Color.GREEN);
-     
-   add(judul);
-        judul.setBounds(50, 10, 300, 50);
-        judul.setForeground(new java.awt.Color(43, 84, 128));
-        judul.setFont(new Font("Algerian",Font.BOLD,28));
-        add(btnLoginAdmin);
-        btnLoginAdmin.setBounds(95, 75, 200, 50);
-        btnLoginAdmin.setForeground(new java.awt.Color(255, 255, 255));
-        btnLoginAdmin.setFont(new Font("Aharoni",Font.BOLD,18));
-        btnLoginAdmin.setBackground(new java.awt.Color(43, 84, 128));
-        add(btnBeliSayur);
-        btnBeliSayur.setBounds(95, 150, 200, 50);
-        btnBeliSayur.setForeground(new java.awt.Color(255, 255, 255));
-        btnBeliSayur.setFont(new Font("Aharoni",Font.BOLD,18));
-        btnBeliSayur.setBackground(new java.awt.Color(43, 84, 128));
-     
-    btnBeliSayur.addActionListener(new ActionListener()
-     {
-         @Override
-         public void actionPerformed(ActionEvent e)
-         {  
-              TambahBarang tambahbarang = new TambahBarang();
-             dispose();
-         }
-     });
-     
-   btnLoginAdmin.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-               LihatBarang lihatbarang = new LihatBarang();
-                dispose();
-            }
-        });
-       
-   }
+    JButton btnTambah = new JButton("Tambah Barang");
+    JButton btnLihat = new JButton("Lihat Barang");
+
+    public ViewMenu() {
+        setTitle("Main Menu");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        
+        setLayout(null);
+        setSize(200,200);
+        add(judul);
+        judul.setBounds(60, 5, 100, 30);
+        
+        add(btnTambah);
+        btnTambah.setBounds(20, 45, 130, 30);
+        
+        add(btnLihat);
+        btnLihat.setBounds(20, 95, 130,30);
+}
 }
